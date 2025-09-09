8f764f29571470712faeaa1ab7710cebdbb7393a
ankitRaj10022 <dannyloyad149@gmail.com>
2025-09-09 18:56:20 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
